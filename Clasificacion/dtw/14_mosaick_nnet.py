4eac979ef1e7c9c13c40cba77469917fec64d51d
aldotapia <aatapia@userena.cl>
2022-06-16 16:00:00 -0400

    added mosaicking results

---
+++ Clasificacion/dtw/14_mosaick_nnet.py
@@ -0,0 +1,21 @@
+import rioxarray as rioxr
+import os
+from rioxarray.merge import merge_arrays
+
+mainpath = '/Users/aldotapia/Documents/1_dtw/scenes/nnet/'
+finalpath = '/Users/aldotapia/Documents/1_dtw/scenes/class results/'
+
+files = os.listdir(mainpath)
+
+rasters = [value for value in files if value.endswith('.tif')]
+
+all_rasters = []
+for raster in rasters:
+    raster_path = os.path.join(mainpath,raster)
+    with rioxr.open_rasterio(raster_path) as rx:
+        all_rasters.append(rx)
+
+r_mosaic = merge_arrays(all_rasters)
+
+fname = os.path.join(finalpath, 'nnet_mosaic.tif')
+r_mosaic.rio.to_raster(fname, compress='LZW')
